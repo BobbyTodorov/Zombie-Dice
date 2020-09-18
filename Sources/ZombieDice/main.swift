176ae7e0bff41fcb3f077309808f35dfaf7bc6f5
BobbyTodorov <btodorov98@gmail.com>
2020-09-18 17:15:10 +0300

    Added Simple Standart Die.

--- Sources/ZombieDice/main.swift
+++ Sources/ZombieDice/main.swift
@@ -1,4 +1,7 @@
 //import ZombieDiceLibrary
+import SimpleDie
 
+let die = SimpleDie<String>(numberOfSides: 3, sidesList: ["asd1","asd2","asd3"])
+print(die.roll())
 //let game = Game()
 //game.run()
