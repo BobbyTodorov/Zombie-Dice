ddf4fb770eb50469d56e3a8fc046ad1fe616a1f6
BobbyTodorov <btodorov98@gmail.com>
2020-09-18 14:02:17 +0300

    Zombie Dice initialization.

--- Package.swift
+++ Package.swift
@@ -14,9 +14,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ZombieDice",
-            dependencies: []),
-        .testTarget(
-            name: "ZombieDiceTests",
-            dependencies: ["ZombieDice"]),
+            dependencies: [])
     ]
 )
